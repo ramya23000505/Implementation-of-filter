0abe56fb9d7e98614167775425e075917100cbf3
ramya23000505 <149370791+ramya23000505@users.noreply.github.com>
2024-09-30 15:54:09 +0530

    Update SHARPENING FILTERS.py

--- SHARPENING FILTERS.py
+++ SHARPENING FILTERS.py
@@ -5,29 +5,25 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-image1=cv2.imread("red.jpg")
-image2=cv2.cvtColor(image1,cv2.COLOR_BGR2RGB)
-kernel=np.ones((11,11),np.float32)/121
-image3=cv2.filter2D(image2,-1,kernel)
-kernel2=np.array([[-1,-1,-1],])
-image3=cv2.filter2D(image2,)
-plt.figure(figsize=(,8))
-plt.subplot()
-plt.imshow(image2)
-plt.title("Original Image")
+image1 = cv2.imread('Ex_5_image.jpeg')
+image2 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
+kernel3 = np.array([[0,1,0], [1, -4,1],[0,1,0]])
+image5 =cv2.filter2D(image2, -1, kernel3)
+plt.imshow(image5)
+plt.title('Laplacian Kernel')
 plt.axis("off")
 plt.subplot(1,2,)
 
 
 # In[5]:Using Laplacian Operator
 
-
-laplacian=cv2.Laplacian(image2,)
-plt.figure(figsize=(8,8))
-plt.subplot()
-plt.imshow(image2)
-plt.title("Original Image")
+laplacian = cv2.Laplacian(image2, cv2.CV_64F)
+plt.figure(figsize=(10, 8))
+plt.subplot(1, 2, 2)
+plt.imshow(laplacian, cmap='gray')
+plt.title("Laplacian Operator Image")
 plt.axis("off")
+plt.show()
 
 
 
